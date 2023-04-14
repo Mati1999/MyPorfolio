d59b22c071dcf95b8c21efbfebb1f7f0188ba38b
Matias Aguilera <aguileramati50@gmail.com>
2023-04-13 23:17:03 -0300

    Adding scroll behavior

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,19 +4,25 @@ import MA from '../static/MA.png';
 import { Link, animateScroll as scroll } from 'react-scroll';
 
 const Header = () => {
+    const scrollToTop = (e) => {
+        e.preventDefault();
+        e.target.className === 'toTop' && scroll.scrollToTop();
+        e.target.className === 'toAboutMe' && scroll.scrollTo(700);
+        e.target.className === 'toTech' && scroll.scrollTo(1600);
+    };
     return (
         <header>
-            <Link className='' to='Presentation' smooth={true}>
-                <img src={MA} alt='' />
+            <Link className='toTop' smooth={true}>
+                <img className='toTop' src={MA} alt='' onClick={scrollToTop} />
             </Link>
             <ul>
-                <li>
-                    <Link className='' to='AboutMe' offset={50} smooth={true}>
+                <li className='toAboutMe' onClick={scrollToTop}>
+                    <Link className='toAboutMe' smooth={true} onClick={scrollToTop}>
                         About me
                     </Link>
                 </li>
-                <li>
-                    <Link className='' to='Tech' offset={-250} smooth={true}>
+                <li className='toTech'>
+                    <Link className='toTech' smooth={true} onClick={scrollToTop}>
                         Tech
                     </Link>
                 </li>
