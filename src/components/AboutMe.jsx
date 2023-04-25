4c93fed2d3c2df29e52e90217486843616fceb8f
Matias Aguilera <aguileramati50@gmail.com>
2023-04-25 20:38:53 -0300

    adding new aboutme img and animation

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import '../scss/AboutMe.scss';
-import aboutMe from '../static/aboutMe.svg';
+import aboutMe from '../static/aboutMe2.svg';
 
 const AboutMe = () => {
     return (
         <div className='AboutMeSection' id='aboutMe'>
-            <img src={aboutMe} alt='' />
+            <img src={aboutMe} alt='' data-aos='zoom-in' />
             <h2 data-aos='zoom-out'>AboutMe</h2>
             <p data-aos='zoom-out'>
                 Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloribus earum quasi, unde numquam nobis atque. Odit dignissimos quibusdam eaque unde repudiandae provident distinctio
