980db8769aa3c551222187a20707bc8c102a1378
Matias Aguilera <aguileramati50@gmail.com>
2023-04-28 21:50:40 -0300

    Adding project gift and link to live demo and code

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -17,8 +17,8 @@ const Projects = () => {
                     <div className='project-content' data-aos='fade-left' data-aos-duration='1000'>
                         <img src={animaBooks} alt='' />
                         <div>
-                            <a href=''>Live</a>
-                            <a href=''>Code</a>
+                            <a href='https://anima-books-gamma.vercel.app/'>Live</a>
+                            <a href='https://github.com/Mati1999/AnimaBooks'>Code</a>
                         </div>
                     </div>
                 </div>
@@ -26,8 +26,8 @@ const Projects = () => {
                     <div className='project-content' data-aos='fade-right' data-aos-duration='1000'>
                         <img src={checkIn} alt='' />
                         <div>
-                            <a href=''>Live</a>
-                            <a href=''>Code</a>
+                            <a href='https://check-in-js.vercel.app/'>Live</a>
+                            <a href='https://github.com/Mati1999/CheckIn-JS'>Code</a>
                         </div>
                     </div>
                     <p className='project-info' data-aos='fade-left' data-aos-duration='1000'>
@@ -43,8 +43,8 @@ const Projects = () => {
                     <div className='project-content' data-aos='fade-left' data-aos-duration='1000'>
                         <img src={portfolio} alt='' />
                         <div>
-                            <a href=''>Live</a>
-                            <a href=''>Code</a>
+                            <a href='https://my-porfolio-roan.vercel.app/'>Live</a>
+                            <a href='https://github.com/Mati1999/MyPorfolio'>Code</a>
                         </div>
                     </div>
                 </div>
