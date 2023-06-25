c44fdbdf98ee08070f6b80d47cacce07c2ad78cf
Matias Aguilera <aguileramati50@gmail.com>
2023-06-25 19:25:20 -0300

    progreso en section experiencia

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -4,7 +4,39 @@ import '../scss/Experience.scss';
 const Experience = () => {
     return (
         <div id='Experience' className='experience'>
-            Experience
+            <h2>Experiencia</h2>
+            <div className='experience-box'>
+                <div className='experience-box--section'>
+                    <h4>Estudios</h4>
+                    <div className='experience-box--content'>
+                        <div className='info'>
+                            <h3>
+                                <b>Full Stack Developer</b> / CoderHouse
+                            </h3>
+                            <span>Agosto 2021 - Octubre 2022</span>
+                            <p>
+                                <b>Actividades:</b>
+                                <ul>
+                                    <li>Resolución de desafíos.</li>
+                                    <li>Creación de proyecto final.</li>
+                                </ul>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div className='experience-box--section'>
+                    <h4>Esperiencia laboral</h4>
+                    <div className='experience-box--content'>
+                        <div className='info'>
+                            <h3>
+                                <b>Salesforce Developer</b> / LabsXD
+                            </h3>
+                            <span>Julio 2022-Actualidad</span>
+                            <p></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
